ebf026602ab6e2ee42a50444c7fe2a5022e7c7c7
naiimoudayet <na.oudayet.im@gmail.com>
2020-03-10 08:32:09 +0100

    initial commit

---
+++ app/src/main/java/io/droidtech/androidserviceexample/NextPageActivity.java
@@ -0,0 +1,14 @@
+package io.droidtech.androidserviceexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NextPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next_page);
+    }
+}
